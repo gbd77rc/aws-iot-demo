922cc145900d6ba0ecb41b74244190716ce963e7
Richard Clarke <rclarke@luxoft.com>
2019-04-09 10:36:47 +0200

    Fixed some issues around messaging.

--- demo/shadow/service.py
+++ demo/shadow/service.py
@@ -1,11 +1,12 @@
+from demo.control.lights import Lights
+from demo.provider.aws import AwsProvider
 import logging
 import json
+from datetime import datetime
 from threading import Thread, Event
 
 logger = logging.getLogger(__name__)
 
-from demo.provider.aws import AwsProvider
-from demo.control.lights import Lights
 
 class Service:
     __shadow = {}
@@ -19,10 +20,10 @@ class Service:
         self.__shadow = provider.get_shadow_handle()
         self.__lights = Lights(self.__config)
         self.__lights.register(self, self.update)
-        logger.debug("Shadow Handle is {}".format(self.__shadow) )       
+        logger.debug("Shadow Handle is {}".format(self.__shadow))
 
     # {"version":5,"timestamp":1554580866,"state":{"green":"off"},"metadata":{"green":{"timestamp":1554580866}}}
-    def __delta_callback(self,payload, responseStatus, token):
+    def __delta_callback(self, payload, responseStatus, token):
         logger.debug("Delta Callback payload is {}".format(payload))
         current = json.loads(payload)
         # Process the desired state first to see if we can set the LEDs to the correct state
@@ -30,7 +31,10 @@ class Service:
             logger.debug("Delta state item is {}".format(key))
             # Check if led or not
             if key == 'lights':
-                self.__lights.toogle(current["state"][key]) 
+                self.__lights.toogle(current["state"][key])
+            if key == 'polling':
+                self.__config["sensor"]["polling"] = current["state"][key]
+                self.update("polling", self.__config["sensor"]["polling"])
 
     def __updated_callback(self, payload, responseStatus, token):
         logger.debug("Updated Callback payload is {}".format(payload))
@@ -44,31 +48,41 @@ class Service:
     # "reported":{"welcome":{"timestamp":1554635548}}},
     # "version":15,"timestamp":1554635569,"clientToken":"4c826256-a9ee-4270-ada8-b9104c8ea7fb"}
     def __get_callback(self, payload, responseStatus, token):
-        logger.debug("Get Callback status is {} payload is {}".format(responseStatus, payload))
+        logger.debug("Get Callback status is {} payload is {}".format(
+            responseStatus, payload))
         current = json.loads(payload)
         # Process the desired state first to see if we can set the LEDs to the correct state
         for key in current["state"]["desired"]:
             logger.debug("Desired state item is {}".format(key))
             # Check if led or not
             if key == 'lights':
-                self.__lights.toogle(current["state"]["desired"][key])    
+                self.__lights.toogle(current["state"]["desired"][key])
+            if key == 'polling':
+                self.__config["sensor"]["polling"] = current["state"]["desired"][key]
+                self.update("polling", self.__config["sensor"]["polling"])
 
     def start(self):
-        self.__shadow.shadowRegisterDeltaCallback(self.__delta_callback)    
-        self.__shadow.shadowGet(self.__get_callback, 30)    
+        self.__shadow.shadowRegisterDeltaCallback(self.__delta_callback)
+        self.__shadow.shadowGet(self.__get_callback, 30)
 
     def stop(self):
         self.__shadow.shadowUnregisterDeltaCallback()
 
     def update(self, event, message):
-        logger.debug("Event [{}] was published with [{}] message".format(event,message))
+        logger.debug(
+            "Event [{}] was published with [{}] message".format(event, message))
         led = None
+        reported = {}
+        reported["state"] = {}
+        reported["state"]["reported"] = {}
+
         if event == "sensors":
-            message = {
-                "temperature": message["temperature"],
-                "humidity": message["humidity"],
-                "cpu_temp": message["cpu_temp"]
-            }
+            reported["state"]["reported"]["sensors"] = {
+                    "temperature": message["temperature"],
+                    "humidity": message["humidity"],
+                    "cpu_temp": message["cpu_temp"]
+                }
+            
             if message["cpu_temp"] > 40:
                 led = {
                     "green": "on"
@@ -77,19 +91,19 @@ class Service:
                     led["yellow"] = 'on'
                 if message["cpu_temp"] > 80:
                     led["red"] = 'on'
-                
-        
-        reported = {
-            "state":{
-                "reported":{
-                    event: message
+            if led is not None:
+                reported["state"]["reported"]["lights"] = led
+                reported["state"]["desired"] = {
+                    "lights": led
                 }
-            }
-        }
+
+        if event == "polling":
+            reported["state"]["reported"]["polling"] = message
+
         if event == "lights":
-            reported["state"]["desired"] = {
-                    event: message
-                } 
+            reported["state"]["reported"]["lights"] = message
+
+        reported["state"]["reported"]["updated"] = (str(datetime.now())).split('.')[0]
 
         j = json.dumps(reported)
         logger.debug("Publishing to Shadow [{}]".format(j))
